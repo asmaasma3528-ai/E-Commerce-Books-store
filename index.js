da0e6510589fcd0154869bacc5a9cb506ef61e81
My-practice-repository <asmaasma3528@gmail.com>
2026-01-04 19:53:50 +0530

    Created controller part of the application

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from "express";
+import ProductController from "./src/controller/products.controller.js";
 
 const server = express();
 
 const PORT = 3500;
 
-server.get("/", (req, res) => {
-    res.end("Welcome to the Books store e-commerce application");
-})
+// server.get("/", (req, res) => {
+//     res.end("Welcome to the Books store e-commerce application");
+// })
+
+//Creating an instance of the product controller
+const productController = new ProductController()
+
+server.get("/", productController.getProducts);
+
+server.use(express.static("src/views"));
 
 server.listen(PORT, () => {
     console.log("The server is listening on the port 3500");
